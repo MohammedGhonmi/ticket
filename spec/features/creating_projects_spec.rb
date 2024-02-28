a934065353bfa475e2498af63c8c9e8f9871c572
MohammedGhonmi <mohammedghonmi@gmail.com>
2024-02-28 11:11:40 +0300

    'Create a new project' feature complete.

---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+RSpec.feature "Users can create new projects" do
+  scenario "with valid attributes" do
+    visit "/projects/new"
+    click_link "New Project"
+    fill_in "project_name", with: "Sublime Text 3"
+    fill_in "project_description", with: "A text editor for everyone"
+    click_button "Create Project"
+    expect(page).to have_content "Project has been created."
+  end
+end
